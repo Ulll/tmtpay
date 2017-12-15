c1e7bc06516e0551061d2b6e39a3fed25c1a50b0
Jo <mod@heiljo.com>
2017-12-15 21:57:59 +0800

    init

---
+++ src/Pay.php
@@ -0,0 +1,46 @@
+<?php 
+
+namespace Tmtpay;
+
+
+class Pay
+{   
+    public $payModel;
+
+    /**
+     * 支付时必需的初始化参数
+     */
+    public function __construct(PayModel $payModel)
+    {
+        $this->payModel = $payModel;
+    }
+
+    /**
+     * 生成订单ID
+     * @return
+     */
+    public function GenerateOrderId()
+    {
+        
+    }
+
+    /**
+     * 修改订单ID
+     * @param  integer $order_id tmtpost订单ID
+     * @return bool
+     */
+    public function ChangePayMethod($orderId)
+    {
+
+    }
+
+    /**
+     * 生成支付凭据
+     * @param  integer $order_id tmtpost订单ID
+     * @return array
+     */
+    public function GenerateProof($orderId)
+    {
+        
+    }
+}
